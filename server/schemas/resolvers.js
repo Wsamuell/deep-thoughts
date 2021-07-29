bdb4d840409133ab943409d9e6044feea54aef36
samuel Odubamowo <wemsamuel@gmail.com>
2021-07-28 23:15:09 -0700

    updated token needs Auth functionality

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,7 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth')
+
+
 const {User, Thought } = require('../models')
 
 const resolvers = {
@@ -20,9 +24,54 @@ const resolvers = {
             .select('-__v -password')
             .populate('friends')
             .populate('thoughts');
+        },
+        me: async (parent, args, context) => {
+            if (context.user) {
+            const userData = await User.findOne({ _id: context.user._id })
+            .select('-__v -password')
+            .populate('friends')
+            .populate('thoughts');
+
+            return userData;
+            }
+            throw new AuthenticationError('Not logged in')
         }
         
 
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            
+            if(!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            const token = signToken(user);
+            return { user, token };
+        },
+        addThought: async (parent, args, context) => {
+            if (context.user) {
+                const thought = await Thought.create({ ...args, username: context.user.username });
+
+                await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $push: {thoughts: thought._id } },
+                    { new: true }
+                );
+                return thought;
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        }
     }
 };
 
